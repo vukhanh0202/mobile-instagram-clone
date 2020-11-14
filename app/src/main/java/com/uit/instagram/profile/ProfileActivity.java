bb57af55907134c8bcd53ea70e73c68fb7467366
Vu Khanh <nguyenvukhanh0202@gmail.com>
2020-11-14 15:37:18 +0700

    Add profileActivity

--- app/src/main/java/com/uit/instagram/profile/ProfileActivity.java
+++ app/src/main/java/com/uit/instagram/profile/ProfileActivity.java
@@ -7,10 +7,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.uit.instagram.R;
-import com.uit.instagram.home.HomeActivity;
 import com.uit.instagram.utils.BottomNavigationViewUtil;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -23,16 +23,35 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: starting");
 
         setupBottomNavigationView();
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: clicked menu item" + item);
+
+                switch (item.getItemId()) {
+                    case R.id.profileMenus:
+                        Log.d(TAG, "onMenuItemClick: profile menu is clicked");
+                }
+                return false;
+            }
+        });
     }
 
     /**
      * Bottom navigationView setup
      */
-    private void setupBottomNavigationView(){
+    private void setupBottomNavigationView() {
         Log.d(TAG, "setupBottomNavigationView: setting up bottom navigationView");
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewUtil.enableNavigation(mContext, bottomNavigationView);
@@ -40,5 +59,12 @@ public class ProfileActivity extends AppCompatActivity {
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+
+        return true;
     }
 }
